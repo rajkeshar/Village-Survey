268db60a6efe51e28f1452d06e1b9e117bb74444
nidhi S <nidhi.sharma@iaastha.com>
2023-02-02 21:58:20 +0530

    feature : add village chnage

--- src/controller/zoneController.ts
+++ src/controller/zoneController.ts
@@ -1,5 +1,6 @@
 import express, { NextFunction, Request, Response } from 'express'
 import mongoose from 'mongoose';
+import { YearlyInstance } from 'twilio/lib/rest/api/v2010/account/usage/record/yearly';
 import zoneModal from '../modal/zoneModal'
 
 export async function addNewZone(req: Request, res: Response) {
@@ -45,13 +46,14 @@ export async function addNewZone(req: Request, res: Response) {
 }
 export async function addNewVillage(req: Request, res: Response) {
     try {
+        let { id } = req.params;
         let { blockUniqueId, villageUniqueId, villageName } = req.body
         if(!blockUniqueId || !villageUniqueId || !villageName) return res.status(400).send({ message: "Please send all required fields" })
 
-        let isExistBlock = zoneModal.find({"blocks" :{$elemMatch:{"blockUniqueId":blockUniqueId}}});
+        let isExistBlock = await zoneModal.find({"_id" :new mongoose.Types.ObjectId(id),"blocks" :{$elemMatch:{"blockUniqueId":blockUniqueId}}});
         if(!isExistBlock) res.status(400).send({ message: "This block id is not exist, Invalid ID" })
 
-        let newVillage = zoneModal.findOneAndUpdate({ "blocks": { $elemMatch: { "blockUniqueId": blockUniqueId } } },
+        let newVillage = await zoneModal.findOneAndUpdate({"_id" :new mongoose.Types.ObjectId(id), "blocks": { $elemMatch: { "blockUniqueId": blockUniqueId } } },
             {
                 $addToSet: {
                     "blocks.$.villages": {
@@ -95,14 +97,19 @@ export async function deleteZone(req: Request, res: Response) {
 }
 export async function deleteBlockOrVillage(req: Request, res: Response) {
     try {
-        let { id , blockName, villageName, blockUniqueId} =  req.params;
+        let { id ,block} =  req.params;
+        let {  blockName, villageName, blockUniqueId } = req.body;
         let zone = await zoneModal.find({_id : new mongoose.Types.ObjectId(id)})
         if(!zone) return res.status(400).send({mesage : "This Id is not exist, Invalid ID"})
-        await zoneModal.findOneAndUpdate({_id : new mongoose.Types.ObjectId(id)},
-        { $pull: { 'blocks':{ 'blockName': blockName }}})
-        await zoneModal.findOneAndUpdate({_id : new mongoose.Types.ObjectId(id),
-            "blocks" :{$elemMatch:{"blockUniqueId":blockUniqueId}}},
-            { $pull: { 'blocks.$.villages':{ 'villageName': villageName }}})
+
+        if( block === 'block' ) {
+            await zoneModal.findOneAndUpdate({_id : new mongoose.Types.ObjectId(id)},
+            { $pull: { 'blocks':{ 'blockName': blockName }}})
+        } else {
+            await zoneModal.findOneAndUpdate({_id : new mongoose.Types.ObjectId(id),
+                "blocks" :{$elemMatch:{"blockUniqueId":blockUniqueId}}},
+                { $pull: { 'blocks.$.villages':{ 'villageName': villageName }}})
+        }
         return res.status(201).send({mesage : "Deleted successfully", success: true})
     } catch (error) {
         res.status(500).send(error);
@@ -111,7 +118,7 @@ export async function deleteBlockOrVillage(req: Request, res: Response) {
 export async function getZoneById(req: Request, res: Response) {
     try {
         let { id } =  req.params;
-        let zone = zoneModal.find({_id : new mongoose.Types.ObjectId(id)})
+        let zone = await zoneModal.find({_id : new mongoose.Types.ObjectId(id)})
         if(!zone) return res.status(400).send({mesage : "This Id is not exist, Invalid ID"})
         return res.status(201).send({mesage : "fetched successfully", success: true, result : zone})
     } catch (error) {
@@ -120,21 +127,53 @@ export async function getZoneById(req: Request, res: Response) {
 }
 export async function getAllDistrict(req: Request, res: Response) {
     try {
-
+        let DistList = await zoneModal.find();
+        if(!DistList) return res.status(201).send({message : "No District found"})
+        let distArray = [] as any;
+        DistList.map(x => {
+            distArray.push({
+                districtName : x.districtName,
+                pincode : x.pincode
+            })
+        })
+        return res.status(201).send({message : "District List", data : distArray})
     } catch (error) {
         res.status(500).send(error);
     }
 }
 export async function getAllBlocks(req: Request, res: Response) {
     try {
-
+       let { distId } = req.params;
+        let blockList = await zoneModal.find({ _id : new mongoose.Types.ObjectId(distId)});
+        if(!blockList) return res.status(201).send({message : "District Id is not found, Invalid ID"})
+        blockList.map( x => {
+            let blockArray=[] as any; 
+            (x.blocks.map(y => {
+                blockArray.push({
+                    blockName : y.blockName,
+                    blockUniqueId : y.blockUniqueId
+                })
+                return res.status(201).send({message : "list of blocks", data: blockArray})
+            }))
+        })
     } catch (error) {
         res.status(500).send(error);
     }
 }
 export async function getAllVillage(req: Request, res: Response) {
     try {
-
+        let { distId, blockId } = req.params;
+        let dist = await zoneModal.find({ _id: new mongoose.Types.ObjectId(distId), "blocks": { $elemMatch: { "blockUniqueId": blockId } } });
+        if (!dist) return res.status(201).send({ message: "District Id is not found, Invalid ID" })
+        let villageArray = [] as any;
+        dist.map(x => {
+            x.blocks.map(y => { if (y.blockUniqueId === blockId) { console.log(y.villages.map(z => {villageArray.push({
+                            villageName: z.villageName as any,
+                            villageUniqueId : z.villageUniqueId as any
+                    })
+            })) }
+            })})
+        return res.status(201).send({ message: "list of blocks", data: villageArray })
     } catch (error) {
         res.status(500).send(error);
     }
@@ -142,7 +181,7 @@ export async function getAllVillage(req: Request, res: Response) {
 export async function getAllZone(req: Request, res: Response) {
     try {
         let { id } =  req.params;
-        let zoneList = zoneModal.find({})
+        let zoneList = await zoneModal.find({})
         return res.status(201).send({mesage : "fetched successfully", success: true, result : zoneList})
     } catch (error) {
         res.status(500).send(error);
--- src/route/zoneRoute.ts
+++ src/route/zoneRoute.ts
@@ -1,15 +1,16 @@
 import express, { application, NextFunction ,Request,Response} from 'express'
-import { addNewZone, addNewVillage, deleteZone, getAllBlocks, getAllDistrict, getAllVillage, getZoneById, updateZone, getAllZone } from '../controller/zoneController';
+import { addNewZone, addNewVillage, deleteZone, getAllBlocks, getAllDistrict, getAllVillage, getZoneById, updateZone, getAllZone, deleteBlockOrVillage } from '../controller/zoneController';
 import {checkRole} from '../utils/user-auth'
 const router = express.Router();
 
 router.post('/addnewzone', addNewZone)
-router.post('/addnewvillage', addNewVillage)
+router.post('/addnewvillage/:id', addNewVillage)
 router.post('/updatezone/:id', updateZone)
 router.get('/getzonebyid/:id', getZoneById)
 router.delete('/deletezone/:id', deleteZone)
-router.get('/getallvillage', getAllVillage)
-router.get('/getallblock', getAllBlocks)
+router.post('/deletebockorvillage/:id/:block', deleteBlockOrVillage)
+router.get('/getallvillage/:distId/:blockId', getAllVillage)
+router.get('/getallblock/:distId', getAllBlocks)
 router.get('/getalldistrict', getAllDistrict)
 router.get('/getallzone', getAllZone)
 
